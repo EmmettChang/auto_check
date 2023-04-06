be2928233bf0b4656c1280149350a4dfc683f2bd
emmett <zhangzenghao@wisdom56.com>
2023-04-06 19:01:09 +0800

    测试提交

---
+++ src/main/java/com/emmett/auto_check/AutoCheckApplication.java
@@ -0,0 +1,13 @@
+package com.emmett.auto_check;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoCheckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoCheckApplication.class, args);
+    }
+
+}
