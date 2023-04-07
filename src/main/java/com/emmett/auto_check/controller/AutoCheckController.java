b6894e7006dfede54db5c7d450b7570d29de0344
emmett <zhangzenghao@wisdom56.com>
2023-04-07 09:22:03 +0800

    项目启动

--- src/main/java/com/emmett/auto_check/controller/AutoCheckController.java
+++ src/main/java/com/emmett/auto_check/controller/AutoCheckController.java
@@ -1,7 +1,9 @@
 package com.emmett.auto_check.controller;
 
+import com.emmett.auto_check.domain.Result;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/autoCheck/")
 public class AutoCheckController {
 
-
+    @PostMapping("/doCheck")
+    public Result<?> importData() {
+        return Result.ok();
+    }
 }
